80935a41556b8d920bd27ea756418fbfa7d755ce
Shreya Prasad <shreyapd06@gmail.com>
2022-02-11 23:19:25 +0530

    Time: 11 ms (24.93%), Space: 13.6 MB (18.56%) - LeetHub

---
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
+       
+        vector<pair<int, int>> sorted;
+       
+        for (int i = 0 ; i < indexes.size(); i++)
+        {
+            sorted.push_back({indexes[i], i});
+        }
+            
+        sort(sorted.rbegin(), sorted.rend());
+        for (auto ind : sorted) {
+            int i = ind.first;
+            string s = sources[ind.second], t = targets[ind.second];
+            if (S.substr(i, s.length()) == s)
+                S = S.substr(0, i) + t + S.substr(i + s.length());
+        }
+        return S;
+    }
+};
